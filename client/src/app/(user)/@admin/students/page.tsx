a01d8102b20f990aced7148269abec2ac7f568c8
Naalaya <117615727+Naalaya@users.noreply.github.com>
2025-03-07 10:26:05 +0700

    Client: Add pagination

--- client/src/app/(user)/@admin/students/page.tsx
+++ client/src/app/(user)/@admin/students/page.tsx
@@ -1,10 +1,27 @@
+import Pagination from "../_components/pagination";
 import StudentPageContainer from "./studentListPage";
+import { listStudents } from "@/app/api/listStudent";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams?: { page?: string };
 }) {
-  const page = searchParams?.page;
-  return <StudentPageContainer page={page} />;
+  const page = searchParams?.page || "1";
+
+  try {
+    const response = await listStudents({ page });
+    const total = response?.result?.total || 1;
+    const limit = response?.result?.limit || 2;
+
+    return (
+      <div className="container m-auto ">
+        <StudentPageContainer page={page} />
+        <Pagination total={total} limit={limit} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    return <div>Đã có lỗi xảy ra, vui lòng thử lại trong giây lát</div>;
+  }
 }
